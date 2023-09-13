e49d44fb248c91a741cd0e15a702b7aa4dd4b0c6
matlavv <mathis.laversin92@gmail.com>
2023-09-13 12:05:51 +0200

    Favicon + dynamic pages navigation

---
+++ travelspace/src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../../components/Header/Header';
+import Packs from '../../components/Pack/Pack';
+
+const Home = () => {
+  return (
+    <div className='home'>
+        <Header />
+        <Packs />
+    </div>
+  )
+}
+
+export default Home
