9ad59de0c66ff651c2a09322549534bce899a1e1
matlavv <mathis.laversin92@gmail.com>
2023-09-14 14:14:32 +0200

    Reservation Form

--- travelspace/src/components/Form/Form.jsx
+++ travelspace/src/components/Form/Form.jsx
@@ -22,10 +22,10 @@ const Form= () => {
       }
     );
   };
-  
+
   return (
     <div id='reserver'>
-        <Typography variant="h3" xs display="flex" justifyContent="center" sx={{ mt: 3 }}>Nous contacter</Typography>
+      <Typography variant="h3" xs display="flex" justifyContent="center" sx={{ mt: 3 }}>Nous contacter</Typography>
        <form ref={form} onSubmit={sendEmail}>
        <Grid xs display="flex" justifyContent="center" alignItems="center" sx={{ mt: 3 }}>       
                 <Box sx={{ display: 'flex', alignItems: 'flex-end', ml: 3  }}>
@@ -34,13 +34,13 @@ const Form= () => {
                 </Box>
                 <Box sx={{ display: 'flex', alignItems: 'flex-end', ml: 3  }}>
                     <MailIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }}/>
-                    <TextField id="input-with-sx" label="E-mail" variant="standard" name="from_email"/>
+                    <TextField id="input-with-sx" label="e-mail" variant="standard" name="from_email"/>
                 </Box>
                 <Box sx={{ display: 'flex', alignItems: 'flex-end', ml: 3  }}>
                     <MailIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }}/>
-                    <TextField id="input-with-sx" label="Message" variant="standard" name="message"/>
+                    <TextField id="input-with-sx" label="message" variant="standard" name="message"/>
                 </Box>
-            </Grid>
+        </Grid>
             <Grid xs display="flex" justifyContent="center" alignItems="center" name sx={{ mt: 4 }} >
                 <Button size="large" type="submit">Envoyer</Button>
             </Grid >
--- travelspace/src/components/Form/FormBooking.jsx
+++ travelspace/src/components/Form/FormBooking.jsx
@@ -1,110 +1,177 @@
 import React from 'react';
 import { TextField } from '@mui/material';
-import User from '../../services/User.service.js';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import { Button, Grid, Typography } from '@mui/material';
+import MailIcon from '@mui/icons-material/Mail';
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import NativeSelect from '@mui/material/NativeSelect';
 
-
-import {
-  MDBInput,
-  MDBCol,
-  MDBRow,
-  MDBCheckbox,
-  MDBBtn,
-  MDBCard,
-  MDBCardBody,
-  MDBCardTitle,
-  MDBCardText,
-} from 'mdb-react-ui-kit';
-import { FormatAlignJustify } from '@mui/icons-material';
-
-const mystyle = {
-  width: "400px",
-  justify:"center",
-  textAlign: "center"
-}
-
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#212F3D',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 const FormBooking = () => {
   return (
-    <form>
-      <div style={{textAlign: "center"}}>
-        <span>Veuillez entrer vos informations personnelles : </span>
-      </div>
-        <MDBInput style={mystyle} label='Nom' type='text' id='form1Example1'  />
-        <MDBInput style={{width: "500px"}} className='mb-4' type='text' id='form1Example2' label='Prénom' />
-        <MDBInput style={{width: "500px"}} className='mb-4' type='email' id='form1Example2' label='Adresse email' />
-     
+    <div id='reserver'>
+      <Typography variant="h3" xs display="flex" justifyContent="center" sx={{ mt: 3 }}>Réserver</Typography>
+       <form>
+       <Grid xs display="flex" justifyContent="center" alignItems="center" sx={{ mt: 3 }}>      
+                <Box sx={{ display: 'flex', alignItems: 'flex-end', ml: 3  }}>
+                    <AccountCircle sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+                    <TextField id="input-with-sx" label="Prenom" variant="standard" name="firstname"/>
+                </Box>
+                <Box sx={{ display: 'flex', alignItems: 'flex-end', ml: 3  }}>
+                    <AccountCircle sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+                    <TextField id="input-with-sx" label="Nom" variant="standard" name="name"/>
+                </Box>
+                <Box sx={{ display: 'flex', alignItems: 'flex-end', ml: 3  }}>
+                    <MailIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }}/>
+                    <TextField id="input-with-sx" label="e-mail" variant="standard" name="=email"/>
+                </Box>
+                <Box sx={{ display: 'flex', alignItems: 'flex-end', ml: 3  }}>
+                    <LocalPhoneIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }}/>
+                    <TextField id="input-with-sx" label="téléphone" variant="standard" name="telephone"/>
+                </Box>
+        </Grid>
 
+        <div id='packs'>       
+        <Grid container spacing={0} sx={{ mt: 10, mr: 2 }} >
+            <Grid xs display="flex" justifyContent="center" alignItems="center">
+                <Item>
+                    <Card sx={{ width: 410, height: 270 }}>
+                        <CardContent>
+                            <Typography variant="h5" color="text.primary" gutterBottom>
+                                Voyage #1
+                            </Typography>
+                            <Typography sx={{ fontSize: 14 }} component="div">
+                                Programme Pioneer
+                            </Typography>
+                            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                                125000$
+                            </Typography>
+                            <Typography variant="body2">
+                            <ul>
+                                    <li>Vol spatial avec l’avion fusée Lynx Mark I</li>
+                                    <li>Contôle médical</li>
+                                    <li>Entrainement Vol acrobatique</li>
+                                    <li>Tenue de vol avec badges</li>
+                            </ul>                           
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Item>
+            </Grid>
+            <Grid xs display="flex" justifyContent="center" alignItems="center">
+                <Item>
+                    <Card sx={{ width: 410, height: 270  }}>
+                        <CardContent>
+                            <Typography variant="h5" color="text.primary" gutterBottom>
+                                Voyage #2
+                            </Typography>
+                            <Typography sx={{ fontSize: 14 }} component="div">
+                                Programme futur astronaut
+                            </Typography>
+                            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                                175000$
+                            </Typography>
+                            <Typography variant="body2">
+                                <ul>
+                                    <li>Vol spatial avec l’avion fusée Lynx Mark II</li>
+                                    <li>Contrôle médical</li>
+                                    <li>Entrainement Vol acrobatique</li>
+                                    <li>Séjour de 3 nuits dans un hôtel de luxe pour 2</li>
+                                    <li>Tenue de vol avec badges</li>
+                                </ul>
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Item>
+            </Grid>
+            <Grid xs display="flex" justifyContent="center" alignItems="center">
+                <Item>
+                    <Card sx={{ width: 410, height: 270  }}>
+                        <CardContent>
+                            <Typography variant="h5" color="text.primary" gutterBottom>
+                                Voyage #3
+                            </Typography>
+                            <Typography sx={{ fontSize: 14 }} component="div">
+                            Programme advanced
+                            </Typography>
+                            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                                200000$
+                            </Typography>
+                            <Typography variant="body2">
+                                <ul>
+                                    <li>Tous les éléments compris dans le programme basique ou avancé</li>
+                                    <li>Production d’un montage vidéo personnalisé: documentaire.</li>
+                                    <li>Tenue d’astronaute</li>
+                                    <li>Entrainement Simulateur de mouvement et de force G</li>
+                                    <li>Rencontre avec le pilote</li>
+                                </ul>
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Item>
+            </Grid>
+    </Grid>
+    </div>
+    <Box xs display="flex" justifyContent="center" sx={{ maxWidth: 170, mt: 5, ml: 88 }}>
+      <FormControl>
+        <InputLabel variant="standard" htmlFor="uncontrolled-native">
+          Programme
+        </InputLabel>
+        <NativeSelect
+          defaultValue={1}
+          inputProps={{
+            
+            name: 'programme',
+            id: 'program',
+          }}
+        >
+          <option value={1}>Pioneer</option>
+          <option value={2}>futur astronaut</option>
+          <option value={3}>advanced</option>
+        </NativeSelect>
+      </FormControl>
+    </Box>
+    <Box xs display="flex" justifyContent="center" sx={{ maxWidth: 170, mt: 5, ml: 88 }}>
+      <FormControl>
+        <InputLabel variant="standard" htmlFor="uncontrolled-native">
+          Date de départ
+        </InputLabel>
+        <NativeSelect
+          defaultValue={1}
+          inputProps={{
+            name: 'date',
+            id: 'date',
+          }}
+        >
+          <option value={1}>12 janvier 2024</option>
+          <option value={2}>15 avril 2024</option>
+          <option value={3}>16 septembre 2024</option>
+        </NativeSelect>
+      </FormControl>
+    </Box>
 
-      <MDBRow className='row-cols-1 row-cols-md-3 g-4'>
-        <MDBCol sm='4'>
-          <MDBCard alignment='center' >
-            <MDBCardBody>
-              <MDBCardTitle>Voyage 1  </MDBCardTitle>
-                <MDBCardText>
-                  <h3>Programme Pioneer </h3>
-                  <h3>125000$</h3>
-                    <ul>
-                      <li>Vol spatial avec l’avion fusée Lynx Mark I</li>
-                      <li>Contrôle médical</li>
-                      <li>Entrainement Vol acrobatique</li>
-                      <li>Tenue de vol avec badges</li>
-                    </ul>   
-                </MDBCardText>
-              <MDBBtn>Selectionner</MDBBtn>
-            </MDBCardBody>
-          </MDBCard>
-        </MDBCol>
-
-        <MDBCol >
-          <MDBCard alignment='center' >
-            <MDBCardBody>
-              <MDBCardTitle>Voyage 2</MDBCardTitle>
-                <MDBCardText>
-                  <h3>Programme futur astronaut</h3>
-                  <h3>175000$</h3>
-                    <ul>
-                      <li>Vol spatial avec l’avion fusée Lynx Mark II</li>
-                      <li>Contrôle médical</li>
-                      <li>Entrainement Vol acrobatique</li>
-                      <li>Séjour de 3 nuits dans un hôtel de luxe pour 2</li>
-                      <li>Tenue de vol avec badges</li>
-                   </ul>   
-                </MDBCardText>
-              <MDBBtn >Selectionner</MDBBtn>
-           </MDBCardBody>
-          </MDBCard>
-        </MDBCol>
-        
-        <MDBCol>
-          <MDBCard alignment='center' >
-            <MDBCardBody>
-              <MDBCardTitle>Voyage 3</MDBCardTitle>
-                <MDBCardText>
-                  <h3>Programme advanced</h3>
-                  <h3>200 000$</h3>
-                   <ul>
-                      <li>Tous les éléments compris dans le programme basique ou avancé</li>
-                      <li>Production d’un montage vidéo personnalisé: documentaire.</li>
-                      <li>Tenue d’astronaute</li>
-                      <li>Entrainement Simulateur de mouvement et de force G</li>
-                      <li>Rencontre avec le pilote</li>
-                  </ul>   
-                </MDBCardText>
-              <MDBBtn  >Selectionner</MDBBtn>
-            </MDBCardBody>
-          </MDBCard>
-        </MDBCol>
-    </MDBRow>
-<br/><br/><br/>
-<div style={{textAlign: "center"}}>
-  <span>Veuillez selectionner une date de depart &nbsp;&nbsp;</span>
-
-  <select >
-    <option value="0">Selectinnez la date :&nbsp;</option>
-    <option value="1">29 fevrier 2035</option>
-  </select>
-</div>
-  </form>
+        <Grid xs display="flex" justifyContent="center" alignItems="center" name sx={{ mt: 4 }} >
+            <Button size="large" type="submit">Envoyer</Button>
+        </Grid >
+    </form>    
+    </div>
   )
 }
 
