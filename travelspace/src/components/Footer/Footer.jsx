b3bf0f48d2b7b5ac87730f02c147acc1225710ab
DontitoSama <jokonito06@gmail.com>
2023-09-14 11:45:12 +0200

    Add Animations

--- travelspace/src/components/Footer/Footer.jsx
+++ travelspace/src/components/Footer/Footer.jsx
@@ -5,10 +5,11 @@ import { Link } from 'react-router-dom'
 const Footer = () => {
   return (
     <div class="container-fluid bg-dark text-white-50 footer mt-5 pt-5 wow fadeIn" data-wow-delay="0.1s">
+        <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
         <div class="container py-5">
             <div class="row g-5">
                 <div class="col-lg-3 col-md-6">
-                    <h1 class="id='titre' fw-bold text-primary mb-4">Travel <span class="id='titre1' text-white">Space</span></h1>
+                    <h1 class="id='titre' fw-bold text-primary mb-4">Travel Space<span class="id='titre1' text-white"></span></h1>
                     <p>Le voyage spatio-temporelle !</p>
                 </div>
                 <div class="col-lg-3 col-md-6">
